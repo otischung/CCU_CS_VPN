4e5e10bb30252e571c1482f82289dd0f5de4b6f2
otischung <my072814638@gmail.com>
2022-02-09 00:00:47 +0800

    add directory path '/workspace'

--- Connect_to_CCU_CS_VPN.sh
+++ Connect_to_CCU_CS_VPN.sh
@@ -1,1 +1,2 @@
-sudo nohup openvpn ~/CCU_CS_VPN/ccucsie.ovpn | cat >> /dev/null & disown
+sudo nohup openvpn ./ccucsie.ovpn | cat >> /dev/null & disown
+# sudo nohup openvpn ~/workspace/CCU_CS_VPN/ccucsie.ovpn | cat >> /dev/null & disown
