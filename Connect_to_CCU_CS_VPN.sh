358ad2d03f60ecab2d6ea91e85bab3f0d9183df9
otischung <my072814638@gmail.com>
2022-03-24 20:45:38 +0800

    add sudo detection

--- Connect_to_CCU_CS_VPN.sh
+++ Connect_to_CCU_CS_VPN.sh
@@ -1,1 +1,12 @@
-sudo nohup openvpn ~/CCU_CS_VPN/ccucsie.ovpn | cat >> /dev/null & disown
+USER=$(id -u)
+if [ $USER != 0 ]; then
+    echo "please use root privilege to run this script."
+    exit 1
+fi
+
+if [ ! -f "/usr/bin/openvpn" ]; then
+    echo "can't find openvpn, please install it manually."
+    exit 1
+fi
+
+nohup openvpn /home/otischung/DATA/workspace/CCU_CS_VPN/ccucsie.ovpn | cat >> /dev/null & disown
